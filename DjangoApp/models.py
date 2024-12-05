2d0af3f71e865f76c1bbeca808f96d2f5009be9e
Froggit2 <165292357+Froggit2@users.noreply.github.com>
2024-12-05 17:17:13 +0300

    Add files via upload

--- DjangoApp/models.py
+++ DjangoApp/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-
 class Users(models.Model):
     login = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+
+class Products(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField(max_length=100)
--- DjangoApp/views.py
+++ DjangoApp/views.py
@@ -1,62 +1,56 @@
-from django.shortcuts import render
-from .models import *
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.forms import UserCreationForm
+from .models import Users, Products
 
 # Create your views here.
-Users_db = Users.objects
 
+Users_db = Users.objects
+Products_db = Products.objects
 
 def glavn(request):
     return render(request, 'glavn.html')
 
-
 def register(request):
     if request.method == "POST":
-        login_input = request.POST.get('login')
-        login_db = Users_db.filter(login=login_input).get()
-        password_1 = request.POST.get('password_1')
-        password_2 = request.POST.get('password_2')
-        if password_1 != password_2:
-            error = 'Пароли должны совпадать!'
-            return render(request, 'register.html',
-                          {'error': error})
-        if login_input == login_db:
-            error = 'Логин не должен совпадать с логином другого пользавателя!'
-            return render(request, 'register.html',
-                          {'error': error})
-        Users_db.create(login=login_input, password=password_2)
-        return render(request, 'register.html')
-    return render(request, 'register.html')
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('http://127.0.0.1:8000/login')
+        else:
+            return render(request, 'register.html', {'form': form})
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
 
+def add_product(request):
+    if request.method == 'POST':
+        name_input = request.POST.get('name')
+        price_input = request.POST.get('price')
+        Products_db.create(name=name_input, price=price_input)
+        message = 'Продукт успешно добавлен!'
+        return render(request, 'add_product.html', {'message': message})
+    return render(request, 'add_product.html')
+
+
+def products(request):
+    products = Products_db.all()
+    return render(request, 'products.html', {'products': products})
 
 def login(request):
     if request.method == "POST":
         login_input = request.POST.get('login')
         password_input = request.POST.get('password')
-        try:
-            login_db = Users.objects.get(login=login_input)
-            password_db = Users.objects.get(password=password_input)
+        user = authenticate(request, username=login_input, password=password_input)
+        if user is not None:
+            auth_login(request, user)
             message = 'Вы успешно вошли!'
-            return render(request, 'login.html',
-                          {'message': message})
-        except:
+            return render(request, 'login.html', {'message': message})
+        else:
             error = 'Неправильный логин или пароль'
-            return render(request, 'login.html',
-                          {'error': error})
+            return render(request, 'login.html', {'error': error})
     return render(request, 'login.html')
 
-
 def logout(request):
-    if request.method == "POST":
-        login_input = request.POST.get('login')
-        password_input = request.POST.get('password')
-        login_db = Users.objects.filter(login=login_input).get()
-        password_db = Users.objects.filter(password=password_input).get()
-        if login_input == login_db and password_input == password_db:
-            login_db.delete()
-            password_db.delete()
-            message = 'Вы успешно вышли!'
-            return render(request, 'logout.html', {'message': message})
-        error = 'Неправильный логин или пароль!'
-        return render(request, 'logout.html',
-                      {'error': error})
+    auth_logout(request)
     return render(request, 'logout.html')
